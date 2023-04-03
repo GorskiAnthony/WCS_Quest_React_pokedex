f1f2a9be21ef242645f961a28f91897e1cf9807f
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-04-03 16:32:48 +0200

    update: add data in component PokemonCard, and test it. Done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,34 @@
+/**
+ * Data en dur dans le component
+ */
+const pokemonList = [
+	{
+		name: "bulbasaur",
+		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
+];
 function PokemonCard() {
+	// Sur ce pokemon RAS ✅
+	// const pokemon = pokemonList[0];
+
+	// Oups, pas d'image sur celui ci 😱 Et oui il n'y a pas d'image
+	const pokemon = pokemonList[1];
 	return (
 		<figure>
-			<img
-				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-				alt="bulbasaur"
-			/>
-			<figcaption>Bulbasaur</figcaption>
+			{
+				/**
+				 * Du coup, je fais une ternaire pour vérifier si image il y a 😁
+				 */
+				pokemon.imgSrc ? (
+					<img src={pokemon.imgSrc} alt={pokemon.name} />
+				) : (
+					<p>???</p>
+				)
+			}
+			<figcaption>{pokemon.name}</figcaption>
 		</figure>
 	);
 }
