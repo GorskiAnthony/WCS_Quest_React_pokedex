8c512f6860f28d2daefa78d525757531f5c07daf
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-04-03 17:09:40 +0200

    add: add state to choice my pokemon (add 2 handler next / prev)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 import "./App.css";
 
@@ -10,15 +11,46 @@ const pokemonList = [
 		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 	},
 	{
+		name: "charmander",
+		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+	{
+		name: "squirtle",
+		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+	{
+		name: "pikachu",
+		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	},
+	{
 		name: "mew",
 	},
 ];
 function App() {
-	const pokemonChoice = pokemonList[0];
+	const [pokemonIndex, setPokemonIndex] = useState(0);
+
+	// J'utilise mon state pour récupérer l'index de mon pokemon
+	// dans le tableau de pokemon (pokemonList)
+	const pokemonChoice = pokemonList[pokemonIndex];
+
+	// S'il clique sur suivant, j'incremente pokemonIndex
+	const handleNext = () => {
+		setPokemonIndex(pokemonIndex + 1);
+	};
+
+	// S'il clique sur precedent, je decremente pokemonIndex
+	const handlePrev = () => {
+		setPokemonIndex(pokemonIndex - 1);
+	};
 
 	return (
 		<div className="App">
+			{/** Je passe ensuite mon pokemon (pokemonChoise) à mon composant */}
 			<PokemonCard pokemon={pokemonChoice} />
+			<div>
+				<button onClick={handlePrev}>Précédent</button>
+				<button onClick={handleNext}>Suivant</button>
+			</div>
 		</div>
 	);
 }
